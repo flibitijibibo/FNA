1d913926a5f64f44ecf4605df849683b97f4c0c7
Ethan Lee <flibitijibibo@gmail.com>
2015-04-25 03:45:06 -0400

    Welcome to FNA Audio, NullDevice!

--- src/Audio/AudioDevice.cs
+++ src/Audio/AudioDevice.cs
@@ -51,20 +51,32 @@ namespace Microsoft.Xna.Framework.Audio
 			{
 				System.Console.WriteLine("ALDevice already exists, overwriting!");
 			}
-			try
+
+			bool disableSound = Environment.GetEnvironmentVariable(
+				"FNA_AUDIO_DISABLE_SOUND"
+			) == "1";
+
+			if (disableSound)
 			{
-				ALDevice = new OpenALDevice();
+				ALDevice = new NullDevice();
 			}
-			catch(DllNotFoundException e)
+			else
 			{
-				System.Console.WriteLine("OpenAL not found! Need FNA.dll.config?");
-				throw e;
-			}
-			catch(Exception)
-			{
-				/* We ignore and device creation exceptions,
-				 * as they are handled down the line with Instance != null
-				 */
+				try
+				{
+					ALDevice = new OpenALDevice();
+				}
+				catch(DllNotFoundException e)
+				{
+					System.Console.WriteLine("OpenAL not found! Need FNA.dll.config?");
+					throw e;
+				}
+				catch(Exception)
+				{
+					/* We ignore and device creation exceptions,
+					 * as they are handled down the line with Instance != null
+					 */
+				}
 			}
 
 			// Populate device list
